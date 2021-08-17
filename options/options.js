478ec93f2bf1f8ca332acdaa9fa09f34934a4702
Crowdbotics <team@crowdbotics.com>
2021-08-17 16:25:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "jolly_term_29884Identifier",
+  url: "https://jolly_term_29884Identifier.botics.co",
+  api: "https://jolly_term_29884Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
